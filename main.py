5460a7305c914e21d4de0792dc4ca0a41731a10e
Alex37321 <149672429+Alex37321@users.noreply.github.com>
2023-11-04 20:18:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import keyboard
+import requests
+from pprint import pprint
+
+
+def get_info_from_ip(ip='127.0.0.1'):
+    try:
+        response = requests.get(url=f'http://ip-api.com/json/{ip}').json()
+        global data
+        data = {
+            'Страна': response.get('country'),
+            'Регион': response.get('regionName'),
+            'Город': response.get('city'),
+            'Широта': response.get('lat'),
+            'Долгота': response.get('lon')
+        }
+    except requests.exceptions.ConnectionError:
+        print('[!] Please check your connection!')
+
+
+def main():
+    ip = input("Введите IP цели:")
+    get_info_from_ip(ip=ip)
+    pprint(data, width=5)
+    print('Нажмите Enter,чтобы закончить...')
+    keyboard.wait('enter')
+
+
+if __name__ == '__main__':
+    main()
